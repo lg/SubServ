b5aba9356f551bdd0fb290157702dd71f48a2497
Larry Gadea <trivex@gmail.com>
2016-08-08 08:29:06 -0600

    removed ipv6 stuff because it was unnecessary

--- subserv.py
+++ subserv.py
@@ -1,8 +1,6 @@
 # - set default editor - EDITOR, VISUAL, or HOMEBREW_EDITOR to your preferred text editor.
 # - pylint
 
-from SubServ.uritools import uricompose
-
 import socket
 import sublime, sublime_plugin
 from threading import Thread
@@ -14,7 +12,6 @@ class SubServ():
 
     settings = sublime.load_settings('SubServ.sublime-settings')
     self.port = settings.get("port", 8080)
-    self.ipv6 = settings.get("ipv6", False)
     self.interface = settings.get("interface", "127.0.0.1")
 
   def start_command(self):
@@ -23,12 +20,8 @@ class SubServ():
 
     # Using serve_forever() is blocking, as such offer it on a new thread
     def threaded_start():
-      # Hey, it's the future, get with it
-      class HTTPServerWithIPv6(HTTPServer):
-        if self.ipv6: address_family = socket.AF_INET6
-
-      self.httpd = HTTPServerWithIPv6((self.interface, self.port), SimpleHTTPRequestHandler)
-      print("SubServ HTTP server ready on %s" % (uricompose(scheme="http", host=self.interface, port=self.port)))
+      self.httpd = HTTPServer((self.interface, self.port), SimpleHTTPRequestHandler)
+      print("SubServ HTTP server ready on %s:%d" % (self.interface, self.port))
       self.httpd.serve_forever()
       print("SubServ HTTP server stopped")
 
